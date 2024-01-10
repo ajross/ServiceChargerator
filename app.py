06ef99bd0a42b6c4e0273913c2522e9fd47b3366
Alasdair Ross <ajross@users.noreply.github.com>
2024-01-10 17:13:25 +0000

    Begin to populate the table of block charges

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, render_template, request
-from models import db, Estates, Blocks
+from models import db, Estates, Blocks, Charges
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://alembic:alembic_pass@localhost:5432/charge_data'
@@ -15,7 +15,32 @@ def get_blocks(estate_id):
     blocks_data = [{'ID': block.ID, 'Block_Name': block.Block_Name} for block in blocks]
     return jsonify({'blocks': blocks_data})
 
-@app.route('/block_charges')
+
+def your_query_function(estate_id, block_id):
+    # Query relevant charge data based on selected estate and block IDs
+    charges = Charges.query.filter_by(Estate_ID=estate_id, Block_ID=block_id).all()
+
+    # Create a dictionary to organize charges by Year_End
+    charges_by_year = {}
+
+    for charge in charges:
+        year_end = charge.Year_End
+
+        if year_end not in charges_by_year:
+            charges_by_year[year_end] = {'Year_End': year_end, 'charges': []}
+
+        charge_data = {'Block_ID': charge.Block_ID,
+                       'Estate_ID': charge.Estate_ID}
+        # Add other charge columns starting with "Block"
+
+        charges_by_year[year_end]['charges'].append(charge_data)
+
+    # Sort the charges by Year_End
+    sorted_charges = sorted(charges_by_year.values(), key=lambda x: x['Year_End'])
+
+    return sorted_charges
+
+@app.route('/block_charges', methods=['GET', 'POST'])
 def block_charges():
     estates = Estates.query.all()
 
