b7e471a51200f294d41c318a8c73016b2c099905
Alasdair Ross <ajross@users.noreply.github.com>
2024-10-15 22:46:53 +0100

    Refactor styles for homepage, even if these aren't final colours

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -17,58 +17,72 @@ const HomePage = () => {
       <NavBar />
       <main>
         <p/>
-        <div className="home-content-container">
-          <div className="home-block-container">
+
+        <section class="home-layout">
+          <div class="home-section">
             <div className="home-content">
               <h2>Welcome to Service Charge Insights!</h2>
-              <p>With this site you can compare your service charges and see whether you are getting value for money.</p>
-              <p>You can see the overall charges for your block and estate, as well as the amount charged to your flat.</p>
-              <p>You can also compare your block or flat charges against others in your borough.</p>
-              <p>We hold data from Greenwich, Lambeth, Lewisham, Southwark, and Tower Hamlets boroughs, and more are being added soon.</p>
-              <p>For each borough, there is an analysis page that shows how your charges compare against the average for the borough, and for similar sized blocks in the borough.</p>
-              <p>Our goal is to help you spot anomalies in your service charge and empower you with the data to succesfully challenge these charges with your council's housing management department.</p>
-              <p>See the page on <Link to="/querying-charges">Querying Charges</Link> for more advice about what to challenge, and how.</p>
+              <h3>Compare your service charges and see whether you are getting value for money.</h3>
+              <h3>See the charges for your block, estate, and your flat.</h3>
+              <h3>Understand how your charges compare to the average in the borough.</h3>
             </div>
           </div>
-          <div className="home-block-container">
+          <div class="querying-section">
+            <div className="querying-content">
+              <h2>Analysis across London</h2>
+              <h3>We hold data from Greenwich, Lambeth, Lewisham, Southwark, and Tower Hamlets boroughs, and more are being added soon.</h3>
+              <h3>Our goal is to help you spot anomalies in your service charge and empower you with the data to succesfully challenge your charges.</h3>
+              <h3>See the page on <Link to="/querying-charges">Querying Charges</Link> for more advice about what to challenge, and how.</h3>
+            </div>
+          </div>
+          <div class="cross-borough-analysis">
             <Link to="/borough-analysis-2022-2023">
               <div className="analysis-content">
                 <h2>Cross-Borough Analysis for 2022-2023</h2>
-                <p>See an analysis of service charges across multiple boroughs in London.</p>
-                <p>We compare all the data to find which borough charges the most and least for common services.</p>
-                <p>Read our analysis to understand how your borough is performing.</p>
-                <p>Consider using this information to lobby your local councillor to hold your council to account.</p>
+                <h3>See an analysis of service charges across multiple boroughs in London.</h3>
               </div>
             </Link>
           </div>
-          <div className="home-links-content-container">
-            <Link to="/greenwich">
-              <div className="block-charges-content">
-                <h2 className="borough-block-title">Greenwich</h2>
+          <div class="borough-blocks">
+            <section class="home-borough-layout">
+              <div>
+                <Link to="/greenwich">
+                  <div className="borough-block">
+                    <h2 className="borough-block-title">Greenwich</h2>
+                  </div>
+                </Link>
               </div>
-            </Link>
-            <Link to="/lambeth">
-              <div className="block-charges-content">
-                <h2 className="borough-block-title">Lambeth</h2>
+              <div>
+                <Link to="/lambeth">
+                  <div className="borough-block">
+                    <h2 className="borough-block-title">Lambeth</h2>
+                  </div>
+                </Link>
               </div>
-            </Link>
-            <Link to="/lewisham">
-              <div className="block-charges-content">
-                <h2 className="borough-block-title">Lewisham</h2>
+              <div>
+                <Link to="/lewisham">
+                  <div className="borough-block">
+                    <h2 className="borough-block-title">Lewisham</h2>
+                  </div>
+                </Link>
               </div>
-            </Link>
-            <Link to="/southwark">
-              <div className="block-charges-content">
-                <h2 className="borough-block-title">Southwark</h2>
+              <div>
+                <Link to="/southwark">
+                  <div className="borough-block">
+                    <h2 className="borough-block-title">Southwark</h2>
+                  </div>
+                </Link>
               </div>
-            </Link>
-            <Link to="/tower-hamlets">
-              <div className="block-charges-content">
-                <h2 className="borough-block-title">Tower Hamlets</h2>
+              <div>
+                <Link to="/tower-hamlets">
+                  <div className="borough-block">
+                    <h2 className="borough-block-title">Tower Hamlets</h2>
+                  </div>
+                </Link>
               </div>
-            </Link>
+            </section>
           </div>
-        </div>
+        </section>
       </main>
       <Footer />
     </div>
